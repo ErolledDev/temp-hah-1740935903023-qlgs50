0cae413332776ea188710ec07fe1ec52f9f2abd3
ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2025-03-03 01:19:25 +0800

    Copy vite.widget.config.ts from hah

---
+++ vite.widget.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist/widget',
+    lib: {
+      entry: 'src/widget/index.tsx',
+      name: 'BusinessChatPlugin',
+      fileName: 'chat',
+      formats: ['umd'],
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
